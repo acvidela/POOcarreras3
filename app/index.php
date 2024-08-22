d8711f56cc6a08485e56c4e219d406e7bec82399
acvidela <acvidela@gmail.com>
2024-08-22 11:31:48 -0300

    se instala la librería Smarty

--- app/index.php
+++ app/index.php
@@ -1,4 +1,10 @@
 <?php
+
+require_once('frontend\lib\smarty\libs\Smarty.class.php');
+
+$smarty = new Smarty\Smarty;                                    //
+
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
     $password = $_POST["password"];
