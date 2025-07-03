ef3bdf3277ae56b10fe806f322803d279be60515
acvidela <acvidela@gmail.com>
2025-07-02 21:12:56 -0300

    Index: reescritura de puntos de entrada

--- app/index.php
+++ app/index.php
@@ -1,2 +1,10 @@
-<?php   
-    header('location:frontend');
+<?php
+// Cargar Smarty
+require_once 'frontend/lib/smarty/libs/Smarty.class.php';
+
+// Cargar el router principal
+require_once 'backend/controllers/router.php';
+
+// Ejecutar enrutamiento
+$router = new Router();
+$router->handleRequest();
